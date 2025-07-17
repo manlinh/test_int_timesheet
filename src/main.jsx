eaa0c303a233280b722b1ae06a27fdc24f91838d
manlinh <135205672+manlinh@users.noreply.github.com>
2025-07-17 21:26:04 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TimesheetEditor from './TimesheetEditor.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TimesheetEditor />
+  </React.StrictMode>,
+)
