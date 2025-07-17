5e66277c9a2ad1ccc48421ce249058a192fee684
manlinh <135205672+manlinh@users.noreply.github.com>
2025-07-17 21:24:49 +0800

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TimesheetEditor from './TimesheetEditor.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TimesheetEditor />
-  </React.StrictMode>,
-)
